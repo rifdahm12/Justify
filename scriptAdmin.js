a3038e5cda4c71ed73b6c2740706f149d135e7ea
karinafs-yo <karinafs@upi.edu>
2026-04-05 20:15:37 +0700

    home admin styling fix karina

--- scriptAdmin.js
+++ scriptAdmin.js
@@ -3,3 +3,10 @@ function toggleMenu() {
     // Toggle class 'tampil'
     menu.classList.toggle("tampil");
 }
+
+function logout() {
+    let yakin = confirm("Apakah anda yakin ingin keluar?");
+    if (yakin) {
+        window.location.href = "homeAdmin.htm";
+    }
+}
